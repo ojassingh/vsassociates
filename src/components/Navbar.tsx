fb423fdd86c5627526e367c0551860292485f7c8
Ojas Singh <ojas.singh02@gmail.com>
2023-03-26 16:24:46 -0400

    bug fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,9 +34,9 @@ export default function Navbar() {
       <ul className="flex gap-4">
         {links.map((link: any) => {
           return (
-            <li id={link.id} className="text-secondary font-medium text-lg px-3 py-2 bg-gray-400/30 rounded-xl">
+            <motion.li whileHover={{scale:1.05, transition: {duration: 0.1}}} id={link.id} className="text-secondary font-medium text-lg px-3 py-2 bg-gray-400/30 rounded-xl">
               <Link href={link.link}>{link.name}</Link>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
